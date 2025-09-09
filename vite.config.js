28a71f20e287cc508ce7f48841e8fe3f66fdff4c
Rikho <rikogaul290@gmail.com>
2025-09-09 21:26:25 +0700

    feat: prepare for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/FrontendMonitoringApp/',  // ✅ wajib buat GitHub Pages
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+        changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
   }
 })
